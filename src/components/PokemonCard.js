058f28a944701e314f509f147641ac60005dcec1
dArunsai <arunsaidyavanapelly@gmail.com>
2025-04-29 10:43:39 +0530

    updated

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,14 +1,25 @@
 import React from "react";
 
 export default function PokemonCard({ pokemon }) {
+  const idPadded = String(pokemon.id).padStart(3, "0");
+  const hdUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${idPadded}.png`;
+
   return (
     <div className="card">
-      <img src={pokemon.sprites.front_default} alt={pokemon.name} />
+      <div className="img-container">
+        <img src={hdUrl} alt={pokemon.name} />
+      </div>{" "}
       <h2>
         #{pokemon.id}{" "}
         {pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}
       </h2>
-      <p>{pokemon.types.map((t) => t.type.name).join(", ")}</p>
+      <div>
+        {pokemon.types.map((t) => (
+          <span key={t.type.name} className={`type ${t.type.name}`}>
+            {t.type.name}
+          </span>
+        ))}
+      </div>
     </div>
   );
 }
